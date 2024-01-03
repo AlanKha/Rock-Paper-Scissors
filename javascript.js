50ecc4792611987c63f1cc362c5c723f40c141b5
AlanKha <khalilialan@gmail.com>
2024-01-03 12:30:14 -0800

    Finished UI and DOM

--- javascript.js
+++ javascript.js
@@ -1,46 +1,109 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const rockBtn = document.querySelector('#rock-button');
+    const paperBtn = document.querySelector('#paper-button')
+    const scissorsBtn = document.querySelector('#scissors-button')
+    const resetBtn = document.querySelector('#reset-button')
+
+    rockBtn.onclick = () => {
+        computerChoice = getComputerChoice()
+        updateScore('Rock', computerChoice);
+    };
+    paperBtn.onclick = () => {
+        computerChoice = getComputerChoice()
+        updateScore('Paper', computerChoice);
+    }
+    scissorsBtn.onclick = () => {
+        computerChoice = getComputerChoice()
+        updateScore('Scissors', computerChoice);
+    }
+    resetBtn.onclick = () =>{
+        resetScoreboard();
+    }
+
+});
+
+function resetScoreboard (){
+    const playerScore = document.querySelector('#player-score');
+    const computerScore = document.querySelector('#computer-score');
+
+    const result = document.querySelector('#result')
+    const resultSubtext = document.querySelector('#result-subtext')
+
+    playerScore.textContent = '0';
+    computerScore.textContent = '0';
+
+    result.textContent = 'Chose your weapon';
+    resultSubtext.textContent = 'First to five wins the game';
+}
+
 function getComputerChoice() {
     const selection = Math.floor(Math.random() * 3);
-    switch(selection) {
+    switch (selection) {
         case 0:
-            return "Rock";
+            return 'Rock';
         case 1:
-            return "Paper";
+            return 'Paper';
         case 2:
-            return "Scissors";
+            return 'Scissors';
     }
 }
 
-function findWinner(userInput, computerInput) {
+function updateScore(choice, computerChoice) {
     if (
-        (userInput === 'Rock' && computerInput === 'Scissors') ||
-        (userInput === 'Scissors' && computerInput === 'Paper') ||
-        (userInput === 'Paper' && computerInput === 'Rock')
-    ) {
-        return `User Wins!\nUser chose ${userInput} and Computer chose ${computerInput}`;
-    } else if (
-        (computerInput === 'Rock' && userInput === 'Scissors') ||
-        (computerInput === 'Scissors' && userInput === 'Paper') ||
-        (computerInput === 'Paper' && userInput === 'Rock')
-    ) {
-        return `Computer Wins!\nUser chose ${userInput} and Computer chose ${computerInput}`;
-    } else {
-        return `It's a Tie!\nUser chose ${userInput} and Computer chose ${computerInput}`;
+        (choice === 'Rock' && computerChoice === 'Scissors') ||
+        (choice === 'Scissors' && computerChoice === 'Paper') ||
+        (choice === 'Paper' && computerChoice === 'Rock')
+    )
+    {
+        updateScoreboard('Player', computerChoice);
+    } 
+    else if (
+        (computerChoice === 'Rock' && choice === 'Scissors') ||
+        (computerChoice === 'Scissors' && choice === 'Paper') ||
+        (computerChoice === 'Paper' && choice === 'Rock')
+    ) 
+    {
+        updateScoreboard('Computer', computerChoice);
+    } 
+    else 
+    {
+        updateScoreboard('Tie', choice);
     }
 }
 
-function validateUserInput(userInput){
-    if( userInput === 'Rock' || userInput === 'Paper' || userInput === 'Scissors'){
-        return true;
-    }
-    else{
-        console.log('Invalid Input! Please enter either Rock Paper or Scissors!');
-        return false;
-    }
-}
+function updateScoreboard(winner, selection) {
+    const result = document.querySelector('#result')
+    const resultSubtext = document.querySelector('#result-subtext')
 
-const userInput = prompt('Please enter either Rock, Paper, or Scissors:');
-const computerInput = getComputerChoice();
+    if (winner === 'Player') {
+        const playerScore = document.querySelector('#player-score');
+        const winSound = document.querySelector('#win-sound')
 
-if(validateUserInput(userInput)){
-    console.log(findWinner(userInput, computerInput));
+        playerScore.textContent = parseInt(playerScore.textContent) + 1;
+
+        winSound.play()
+
+        result.textContent = "You Win!"
+        resultSubtext.textContent = `Computer chose ${selection}`
+
+    }
+    else if(winner === 'Computer'){
+        const loseSound = document.querySelector('#lose-sound')
+        const computerScore = document.querySelector('#computer-score')
+
+        computerScore.textContent = parseInt(computerScore.textContent) + 1
+
+        loseSound.play()
+
+        result.textContent = "Computer Wins!"
+        resultSubtext.textContent = `Computer Chose ${selection}`
+    }
+    else if(winner === 'Tie'){
+        const tieSound = document.querySelector('#tie-sound');
+        
+        tieSound.play()
+
+        result.textContent = "It's a Tie!"
+        resultSubtext.textContent = `Both of you chose ${selection}`
+    }
 }
